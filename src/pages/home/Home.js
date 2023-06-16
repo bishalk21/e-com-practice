cb44bb883e3c987549171d99b3e7a15ec699ac0b
Your Name <karkibishal00@gmail.com>
2023-06-16 10:45:11 +1000

    banner section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./home.styles.css";
 import Hero from "./components/hero/Hero";
 import ProudProducts from "./components/proud-product/ProudProducts";
+import Banner from "./components/banner/Banner";
+import banner1 from "./assets/banner/banner1.jpg";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <ProudProducts />
+      <Banner
+        title="Creative harmonious living"
+        text="g-shop products are all made to standard sizes so that you can mix and match them freely."
+        img={banner1}
+      />
     </>
   );
 };
